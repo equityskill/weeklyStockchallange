749e54aded08f426332be8be735f0ac7a4da99b7
equityskill <equityskill@gmail.com>
2025-12-26 03:32:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+/* ===== GLOBAL CONFIG ===== */
+
+const CONFIG = {
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz2mEQnX49GHoOPXvadmId8ETWMOjLkSLPQ00yFh_f3U9XgAfiAfpO2BXd5FLJD_7Lx/exec",
+  /*APP_NAME: "EquitySkill"*/
+};
