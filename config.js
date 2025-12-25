441a96dab18e2362afb4b01543793f49e8774fdb
equityskill <equityskill@gmail.com>
2025-12-26 03:29:28 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-/* ===== GLOBAL CONFIG ===== */
-
-const CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw8TmUpr5V6qGvnlVqnsxacmkWpFqgyBHdH6j4BOjNsWHH39taim6x9ArLI9yH2KBiX/exec",
-  /*APP_NAME: "EquitySkill"*/
-};
